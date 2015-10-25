21f7d8f7a932f116099898beed2635abd839b68f
Taiki Nakaoka <nakaokataiki@gmail.com>
2015-10-25 18:42:57 +0900

    implement saving draft automatically

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@ var Notes = function(){
         localStorage.writeDownNotes = JSON.stringify([]);
     };
 
+    if (!localStorage.tempNote){
+        localStorage.tempNote = JSON.stringify([]);
+    };
+
+    this.setTempNote = function(obj){
+        var data = JSON.parse(localStorage.tempNote);
+        data[0] = obj;
+        localStorage.tempNote = JSON.stringify(data);
+    }
+
+    this.getTempNote = function(){
+        var data = JSON.parse(localStorage.tempNote);
+        return data[0];
+    }
+
     this.addNote = function(obj){
         var data = JSON.parse(localStorage.writeDownNotes);
         data.unshift(obj);
@@ -31,9 +46,26 @@ var ViewModel = function(){
 
     this.notes = new Notes();
 
-    this.newNote = ko.observable();
+    this.newNote = ko.observable(
+        function(){
+            var tempNote = that.notes.getTempNote();
+            var lastSaveNote = that.notes.getAllNotes()[0];
+            if(tempNote.content == lastSaveNote.content){
+                return "";
+            }else{
+                return tempNote.content
+            }
+        }()
+    );
 
     this.allNotes = ko.observableArray(this.notes.getAllNotes());
+
+    this.saveTempNote = function(){
+        var tempNoteObj = {
+            content: that.newNote()
+        };
+        that.notes.setTempNote(tempNoteObj);
+    }
 
     this.addNote = function(){
         var newNoteObj = {
